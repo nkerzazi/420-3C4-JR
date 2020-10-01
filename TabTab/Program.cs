d631f9c464f1a1fc78c1dfe46a3bb1acdfe30c30
noker <nkerzazi@gmail.com>
2020-10-01 16:35:24 -0400

    Changement de code pour hashTable et Dictionary

--- TabTab/Program.cs
+++ TabTab/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace TabTab
 {
@@ -10,39 +9,40 @@ namespace TabTab
     {
         static void Main(string[] args)
         {
-            //Creation d'un tableau multi-dimentionel
-            string[] tabEtudiants = new string[3];
-            tabEtudiants[0] = "Maximo";
-            tabEtudiants[1] = "Jonathan";
-            tabEtudiants[2] = "Boby";
+            Dictionary<string, string> ma_Dictionary = new Dictionary<string, string>();
+
+            //Ajout d'elements dans ma_dictionary
+            ma_Dictionary.Add("1", "Un");
+            ma_Dictionary.Add("2", "Deux");
+            ma_Dictionary.Add("3", "Trois");
+
+            Hashtable ma_HashTable = new Hashtable();
+
+            //Ajout d'elements dans ma_dictionary
+            ma_HashTable.Add("A1", "A Un");
+            ma_HashTable.Add("A2", "A Deux");
+            ma_HashTable.Add("A3", "A Trois");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //   Console.WriteLine("--Ajout d un element " + i);
+            //  ma_Dictionary.Add("ok" + i, "client" + i);
+            // }
 
 
-            string[][] TabTabArray = new string[3][];
-
-            TabTabArray[0] = new string[3];
-            TabTabArray[1] = new string[1];
-            TabTabArray[2] = new string[2];
-
-            TabTabArray[0][0] = "Technique Cegep";
-            TabTabArray[0][1] = "Baccalaureat";
-            TabTabArray[0][2] = "Maitrise";
-
-            TabTabArray[1][0] = "Technique Cegep";
-
-            TabTabArray[2][0] = "Technique Cegep";
-            TabTabArray[2][1] = "Baccalaureat";
-
-            for (int i=0; i<TabTabArray.Length; i++)
+            // afficher les elements de ma_Hashtable
+            foreach (KeyValuePair<string, string> entry in ma_Dictionary)
             {
-                string[] tabinternArray = TabTabArray[i];
-                Console.WriteLine(tabEtudiants[i]);
-                Console.WriteLine("---------------");
-                for (int j = 0; j < tabinternArray.Length; j++)
-                {
-                    Console.WriteLine(tabinternArray[j]);
-                }
-                Console.WriteLine("****");
+                Console.WriteLine("la clé: - {0} et la valeur : - {1}",
+                                entry.Key, entry.Value);
             }
+
+            foreach (DictionaryEntry entry in ma_HashTable)
+            {
+                Console.WriteLine("la clé: - {0} et la valeur : - {1}",
+                                entry.Key, entry.Value);
+            }
+
             Console.ReadKey();
 
         }
